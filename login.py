eb8d78be9df9e7ab314dd1298ca550811ad30d95
mbuturak <01buturak@gmail.com>
2023-08-07 16:59:07 +0300

    ..

--- login.py
+++ login.py
@@ -6,22 +6,27 @@ import subprocess
 correct_username = "mbuturak"
 correct_password = "123456"
 
-# Streamlit arayüzünü oluştur
-st.title("Giriş Ekranı")
+def main():
+    # Streamlit arayüzünü oluştur
+    st.title("Giriş Ekranı")
+    
+    # Kullanıcı adı ve parola girişi
+    username = st.text_input("Kullanıcı Adı")
+    password = st.text_input("Parola", type="password")
 
-# Kullanıcı adı ve parola girişi
-username = st.text_input("Kullanıcı Adı")
-password = st.text_input("Parola", type="password")
+    # Giriş düğmesi
+    if st.button("Giriş"):
+        if username == correct_username and password == correct_password:
+            st.success("Giriş başarılı! Uygulamaya erişiminiz sağlandı.")
 
-# Giriş düğmesi
-if st.button("Giriş"):
-    if username == correct_username and password == correct_password:
-        st.success("Giriş başarılı! Uygulamaya erişiminiz sağlandı.")
+            # diabetes_prediction_app.py dosyasını çalıştır
+            subprocess.run(["streamlit", "run", "diabetes_prediction_app.py"])
 
-        # diabetes_prediction_app.py dosyasını çalıştır
-        subprocess.run(["streamlit", "run", "diabetes_prediction_app.py"])
+            # Uygulama sonlandığında buraya dönecek
+            st.warning("Uygulama sonlandı.")
+        else:
+            st.error("Kullanıcı adı veya parola yanlış. Lütfen tekrar deneyin.")
 
-        # Uygulama sonlandığında buraya dönecek
+if __name__ == "__main__":
+    if main():
         st.warning("Uygulama sonlandı.")
-    else:
-        st.error("Kullanıcı adı veya parola yanlış. Lütfen tekrar deneyin.")
