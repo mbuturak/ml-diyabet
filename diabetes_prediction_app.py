43450d4279d6c3682a865efd33de4dfe3967fb99
mbuturak <01buturak@gmail.com>
2023-08-21 15:23:33 +0300

    ..

--- diabetes_prediction_app.py
+++ diabetes_prediction_app.py
@@ -35,38 +35,38 @@ bmi = st.sidebar.number_input('Vücut Kitle İndeksi', value=0.0)
 diabetes_pedigree = st.sidebar.number_input('Diyabet Soyağacı Fonksiyonu', value=0.0)
 age = st.sidebar.number_input('Yaş', value=0)
 
-# Tahmin işlemi ve yeni kayıtları eklemek
-if st.sidebar.button('Hesapla'):
+# # Tahmin işlemi ve yeni kayıtları eklemek
+# if st.sidebar.button('Hesapla'):
     
-    if all([pregnancies == 0, glucose == 0, blood_pressure == 0, skin_thickness == 0, insulin == 0, bmi == 0, diabetes_pedigree == 0, age == 0]):
-        st.warning("En az 1 değer girmeniz gerekmektedir.")
-    else:
-        user_input = np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree, age]])
-        prediction = model.predict(user_input)
+#     if all([pregnancies == 0, glucose == 0, blood_pressure == 0, skin_thickness == 0, insulin == 0, bmi == 0, diabetes_pedigree == 0, age == 0]):
+#         st.warning("En az 1 değer girmeniz gerekmektedir.")
+#     else:
+#         user_input = np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree, age]])
+#         prediction = model.predict(user_input)
 
-        if prediction[0] == 0:
-            result = "Diyabet değil"
-        else:
-            result = "Diyabet"
+#         if prediction[0] == 0:
+#             result = "Diyabet değil"
+#         else:
+#             result = "Diyabet"
 
-        # Tahmin sonucunu renk ve yazı stili ile göster
-        if prediction[0] == 1:
-            st.write('Tahmin Sonucu:', f'<span style="color:red;font-size:20px">Diyabet riski taşımaktasınız</span>', unsafe_allow_html=True)
-        else:
-            st.write('Tahmin Sonucu:', f'<span style="color:green;font-size:20px">Diyabet riski taşımamaktasınız</span>', unsafe_allow_html=True)
+#         # Tahmin sonucunu renk ve yazı stili ile göster
+#         if prediction[0] == 1:
+#             st.write('Tahmin Sonucu:', f'<span style="color:red;font-size:20px">Diyabet riski taşımaktasınız</span>', unsafe_allow_html=True)
+#         else:
+#             st.write('Tahmin Sonucu:', f'<span style="color:green;font-size:20px">Diyabet riski taşımamaktasınız</span>', unsafe_allow_html=True)
 
 
-# Veri kümesinin boyutlarını göster
-total_records = len(diabetes)
-st.write(f'Veri kümesinde toplam {total_records} kayıt bulunmaktadır.')
+# # Veri kümesinin boyutlarını göster
+# total_records = len(diabetes)
+# st.write(f'Veri kümesinde toplam {total_records} kayıt bulunmaktadır.')
 
-# Eğitim ve test kümesi boyutlarını göster
-train_records = len(x_train)
-test_records = len(x_test)
+# # Eğitim ve test kümesi boyutlarını göster
+# train_records = len(x_train)
+# test_records = len(x_test)
 
-# Model skorunu hesapla
-score = model.score(x_test, y_test)
-cv_scores = cross_val_score(model, x, y, cv=5)  # 5 katlı çapraz doğrulama
+# # Model skorunu hesapla
+# score = model.score(x_test, y_test)
+# cv_scores = cross_val_score(model, x, y, cv=5)  # 5 katlı çapraz doğrulama
 
 # # Düğmeye tıklanınca ağacın metin tabanlı açıklamalarını göster/gizle
 # show_tree = st.checkbox('Karar Ağacı Açıklamalarını Göster/Gizle')
